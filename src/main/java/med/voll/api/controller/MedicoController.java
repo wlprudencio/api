8bd6642988de483a568cd67b334f6f22fa15afdf
wlprudencio <wl.prudencio@gmail.com>
2023-10-11 03:08:23 -0300

    Fiz atualizacoes, aonde correu de fazer validações em geral em toda a parte do controlle Medico. A

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,13 +3,17 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.medico.*;
+import med.voll.api.domain.medico.DadosListagemMedico;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.util.UriComponentsBuilder;
 
 
 @RestController
@@ -18,28 +22,56 @@ public class MedicoController {
     @Autowired
     private MedicoRepository repository;
 
+    //Metodo 201 Cadastro: Created" é utilizado como resposta de sucesso,
+    // indica que a requisição foi bem sucedida e que um novo recurso foi criado.
+
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody  @Valid  DadosCadastroMedico dados){
+    public ResponseEntity cadastrar(@RequestBody  @Valid  DadosCadastroMedico dados, UriComponentsBuilder uriBuilder){
+       var medico = new Medico(dados);
+        repository.save (medico);
 
-        repository.save(new Medico(dados));
+      var uri = uriBuilder.path("/medico/{id}").buildAndExpand(medico.getId()).toUri();
+        return  ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
     }
+
+    // Metodo 200 Listagem: "é a resposta de status de sucesso que indica que a requisição foi bem sucedida."
     @GetMapping
-    public Page<DadosListagemMedico> listar( @PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+    public ResponseEntity <Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+         var page = repository.findAll(paginacao).map(DadosListagemMedico::new);
+         return ResponseEntity.ok(page);
     }
+
+    // Metodo 200 Atualizar: "é a resposta de status de sucesso que indica que a requisição foi bem sucedida."
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
 
        var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
 
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+
     }
+
+    //Metodo 2004 Excluir: No Content indica que a solicitação foi bem sucedida e o cliente
+    // não precisa sair da página atual
      @DeleteMapping("/{id}")
      @Transactional
-     public void excluir(@PathVariable Long id){
-        repository.deleteById(id);
+     public ResponseEntity excluir(@PathVariable Long id){
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
 
+        return ResponseEntity.noContent().build();
+    }
+
+    //Metodo 200 detalhar: Created" "é a resposta de status de sucesso
+    // que indica que a requisição foi bem sucedida."
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id){
+        var medico = repository.getReferenceById(id);
+
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 }
--- src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
@@ -1,15 +1,15 @@
-package med.voll.api.medico;
+package med.voll.api.domain.medico;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.domain.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
 
-        @NotBlank
+        @NotBlank(message = "Nome é obrigatório")
         String nome,
         @NotBlank
         @Email
